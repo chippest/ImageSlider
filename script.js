4426e579372aefabfeadde0b6e89be59b314fb34
Chipp <ibtisamaleem6@gmail.com>
2025-01-18 20:59:49 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector(".slider");
+const images = Array.from(slider.children);
+
+function updateClasses(direction) {
+  if (direction === "left") {
+    const first = images.shift();
+    images.push(first);
+  } else if (direction === "right") {
+    const last = images.pop();
+    images.unshift(last);
+  }
+
+  images.forEach((img, index) => {
+    img.className = "";
+    if (index === 0) img.classList.add("left");
+    else if (index === 1) img.classList.add("center");
+    else if (index === 2) img.classList.add("right");
+    else img.classList.add("back");
+  });
+}
+
+document
+  .getElementById("left")
+  .addEventListener("click", () => updateClasses("right"));
+document
+  .getElementById("right")
+  .addEventListener("click", () => updateClasses("left"));
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    if (img.classList.contains("left")) {
+      updateClasses("right");
+    } else if (img.classList.contains("right")) {
+      updateClasses("left");
+    }
+  });
+});
